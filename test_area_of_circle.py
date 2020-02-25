034285deb7dece68730e71f3a0522f2e0a23a955
alexsincai <alex.sincai@yahoo.co.uk>
2020-02-25 13:49:35 +0200

    Made some changes to area and test

--- test_area_of_circle.py
+++ test_area_of_circle.py
@@ -5,7 +5,7 @@ from math import pi
 
 class TestComputeArea(unittest.TestCase):
     def test_area(self):
-        '''Checks that the area is correctly computed for positive ints'''
+        '''Checks that the area is correctly computed for positive integers'''
         self.assertAlmostEqual(compute_area(1), pi)
         self.assertAlmostEqual(compute_area(0), 0)
         self.assertAlmostEqual(compute_area(5.4), 5.4 * 5.4 * pi)
